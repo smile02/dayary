7752007d9fc14d8a8b35ff296b33eeca7d16d4a8
ko hyeonjin <gosu12002@naver.com>
2018-06-18 11:23:19 +0900

    init project

---
+++ src/main/java/com/inc/dayary/DayaryApplication.java
@@ -0,0 +1,12 @@
+package com.inc.dayary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayaryApplication.class, args);
+	}
+}
